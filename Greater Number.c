c01ceaad5fcb58bc43c6ec1bb4e03f566d2fd57c
pshyticyash <72242996+pshyticyash@users.noreply.github.com>
2022-03-22 23:20:38 +0530

    Update Greater Number.c

--- Greater Number.c
+++ Greater Number.c
@@ -2,12 +2,12 @@
 #include<stdio.h>
 int main(){
   int num1,num2,num3;
-  scanf("%d",&num1"); 
-  scanf("%d",&num2");     
-  scanf("%d",&num3");
+  scanf("%d",&num1); 
+  scanf("%d",&num2);     
+  scanf("%d",&num3);
         
-      if (num1>num2  && num1>num3){printf("Number 1 Is The Greatest (%d),num1");
-      if (num2>num1  && num2>num3){printf("Number 2 Is The Greatest (%d),num2");
-      if (num3>num2  && num3>num1){printf("Number 3 Is The Greatest (%d),num3");
+          if (num1>num2  && num1>num3){printf("Number 1 Is The Greatest (%d)",num1);}
+          if (num2>num1  && num2>num3){printf("Number 2 Is The Greatest (%d)",num2);}
+          if (num3>num2  && num3>num1){printf("Number 3 Is The Greatest (%d)",num3);}
 return 0;
 }
